ca040ef1d9c73be7ae1ce1185a84ee2b891b8da9
student <student@158.197.35.82>
2018-09-19 08:04:38 +0200

    pridany komentar

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,9 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		// toto je skusaci komentar
+		
+		
 		System.out.println("Hello Wo" + "rld");
 		for (int i = 0; i < 10; i++) {
 			System.out.println("git je super");
