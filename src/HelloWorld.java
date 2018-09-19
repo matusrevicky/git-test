86fd3d8ca74bf5fd0ea9a8b9ca838c14ccc88f92
student <student@158.197.35.82>
2018-09-19 08:11:06 +0200

    top

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// toto je skusaci komentar fg sd fd sdf
 		
+		///sd s d sd s d
+		
 		
 		System.out.println("Hello Wo" + "rld");
 		for (int i = 0; i < 10; i++) {
