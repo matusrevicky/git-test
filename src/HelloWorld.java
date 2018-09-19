d84801c93944492a1073b62afb25b3d704d11664
student <student@158.197.35.82>
2018-09-19 07:32:48 +0200

    10x git je super
    pridany for cyklus

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,9 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello Wo" + "rld");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("git je super");
+		}
 	}
 }
